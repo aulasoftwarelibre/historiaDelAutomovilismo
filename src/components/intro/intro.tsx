4ef45d4c23ba7c6f53e6f54cccdd136d302e4983
David19 <davidsanchezpalomino200603@gmail.com>
2025-09-17 18:21:48 +0200

    fixed: intro component

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -2,46 +2,51 @@ import Image from "next/image";
 
 export const Funcion = () => {
   return (
-    <div className="text-7xl ml-40 mt-35 font-semibold not-italic text-black no-underline">
-      Década en marcha:
-      <br />
-      Aula del Motor y
-      <br />
-      Mujer en la Pole
-      <Image
-        src="/images/Logo NOV 500x500_page-0001.jpg"
-        alt="error no se encontro"
-        width={450}
-        height={450}
-        className="mt-[-210px] ml-200"
-      />
-      <div className="text-3xl ml-15 mt-[-110px] font-normal not-italic text-[#787ff6]">
-        NOVIEMBRE 14-24
+    <div className="md:text-3xl lg:text-7xl text-xl font-semibold not-italic text-black no-underline bg-white md:grid grid-cols-3">
+
+      <div id="titulo" className="md:justify-self-start justify-self-center align-middle p-[10%] md:col-span-2">
+        <p>
+          Década en marcha:<br />Aula del Motor y<br />Mujer en la Pole
+        </p>
+        <div className="grid grid-flow-col">
+            <div className="lg:w-32 md:w-22 w-16 mt-10 h-1" style={{ backgroundColor: "#787ff6" }}></div>
+            <div className="lg:w-32 md:w-22 w-16 mt-10 h-1" style={{ backgroundColor: "#787ff6" }}></div>
+        </div>
       </div>
-      <div className="text-3xl ml-15 mt-10 font-normal not-italic text-[#787ff6]">
-        PALACIO DE LA MERCED
-        <br />
-        DIPUTACIÓN DE CÓRDOBA
+
+      <div id="logo" className="mb-[100] justify-self-center max-w-50 md:p-20 md:max-w-1000">
+        <Image
+          src="/images/Logo NOV 500x500_page-0001.jpg"
+          alt="logo decada en marcha"
+          //MODIFICAR EL WIDTH/HEIGHT
+          width={450}
+          height={450}
+          className="justify-self-start"
+        />
       </div>
-      <div className="ml-2 mt-[-140px] w-6 h-6 rotate-(-90) border-t-4 border-r-4 text-[#787ff6]">
-        <div className="ml-(-1) mt-[73px] w-6 h-6 rotate-(-90) border-t-4 border-r-4 text-[#787ff6]"></div>
-        <div
-          className="w-6 h-1 mt-[-90px] rotate-135"
-          style={{ backgroundColor: "#787ff6" }}
-        ></div>
-        <div
-          className="w-6 h-1 mt-[72.2px] rotate-135"
-          style={{ backgroundColor: "#787ff6" }}
-        ></div>
-      </div>
-      <div
-        className="ml-2 mt-[-100px] w-32 h-1"
-        style={{ backgroundColor: "#787ff6" }}
-      ></div>
-      <div
-        className="ml-80 mt-[-4px] w-32 h-1"
-        style={{ backgroundColor: "#787ff6" }}
-      ></div>
+
+      <div id="textitos" className="pl-[10%] md:mt-[-10vw] lg:mt-[-8vw] mt-[-10%] mb-[10%]">
+
+          <div id="primer-texto" className="grid grid-cols-6 p-10">
+            <div className="col-span-1">
+              <div className="w-6 h-6 rotate-(-90) border-t-4 border-r-4 text-[#787ff6]"></div>
+              <div className="w-6 h-1 mt-[-13] rotate-135" style={{ backgroundColor: "#787ff6" }}></div>
+            </div>
+            <div className="lg:text-3xl md:text-lg text-s font-normal not-italic text-[#787ff6] col-span-5 pl-4">
+              NOVIEMBRE 14-24
+            </div>
+          </div>
+
+          <div id="segundo-texto" className="grid grid-cols-6 p-10">
+            <div className="col-span-1">
+              <div className="w-6 h-6 rotate-(-90) border-t-4 border-r-4 text-[#787ff6]"></div>
+              <div className="w-6 h-1 mt-[-13] rotate-135" style={{ backgroundColor: "#787ff6" }}></div>
+            </div>
+            <div className="lg:text-3xl md:text-xl text-s font-normal not-italic text-[#787ff6] col-span-5 pl-4">
+              PALACIO DE LA MERCED<br />DIPUTACIÓN DE CÓRDOBA
+            </div>
+          </div>
+        </div>
     </div>
   );
 };
